b4c6951b2653e90fc7ae1e06df2bd72411fdb8d3
acostapazo <acosta@alicebiometrics.com>
2020-01-09 18:17:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 PACKAGE_NAME = "meiga"
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 # The text of the README file
 with open(os.path.join(CURRENT_DIR, "README.md")) as fid:
