168f7ccdcf6ae96b0ca231992c6d6d4d49b2a0f7
acostapazo <acosta@alicebiometrics.com>
2022-10-06 11:53:03 +0200

    Feature/improve doc (#30)
    * fix(github-actions): try to reproduce error on production leaked by a lume dependency
    
    * fix: try to solve error importing ParamSpec from python versions above 3.10. Add ass requirement typing_extensions for python versions below 3.8
    
    * fix: add typing-extension requirement for python below 3.10
    
    * chore: update lume.yml to install requirements (now it is not empty)
    
    * fix(github-actions): install current version of meiga on test-imports github action job
    
    * fix: add requirements/requirements.txt to MANIFEST.in
    
    * chore: add 3.11-dev to python-version matrix to try it
    
    * chore(tests): add some testing to assert some examples given in the documentation

---
+++ tests/unit/doc/test_example_without_meiga.py
@@ -0,0 +1,25 @@
+import pytest
+
+from tests.unit.doc.example_without_meiga import (
+    NoSuchKey,
+    TypeMismatch,
+    string_from_key,
+)
+
+
+@pytest.mark.unit
+class TestExampleWithoutMeiga:
+    dictionary = {"key1": "value", "key2": 2}
+
+    def should_return_a_str(self):
+        value = string_from_key(dictionary=self.dictionary, key="key1")
+        assert isinstance(value, str)
+
+    def should_raises_non_such_key_exception(self):
+        with pytest.raises(NoSuchKey):
+            _ = string_from_key(dictionary=self.dictionary, key="invalid_key")
+
+    def should_raises_type_mismatch_exception(self):
+        with pytest.raises(TypeMismatch):
+            value = string_from_key(dictionary=self.dictionary, key="key2")
+            assert not isinstance(value, str)
