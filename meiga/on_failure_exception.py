2911b59f2ead1aa391e4f60a3040b6b6d8feaf0d
acostapazo <acosta@alicebiometrics.com>
2023-05-03 23:38:15 +0200

    feat(typing): add type-hint to OnFailureException result attribute (#54)
    * feat(typing): add type-hint to OnFailureException result attribute

--- meiga/on_failure_exception.py
+++ meiga/on_failure_exception.py
@@ -1,7 +1,14 @@
+from typing import TYPE_CHECKING
+
 from meiga.error import Error
+
+if TYPE_CHECKING:
+    from meiga.alias import AnyResult
 
 
 class OnFailureException(Error):
-    def __init__(self, result) -> None:
-        Exception.__init__(self)
+    result: "AnyResult"
+
+    def __init__(self, result: "AnyResult") -> None:
         self.result = result
+        Exception.__init__(self)
