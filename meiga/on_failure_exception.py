23ee60d8235e4f9bbdf87e98c7c9a765aedd39c0
acostapazo <acosta@alicebiometrics.com>
2023-05-03 23:26:27 +0200

    fix: circular import

--- meiga/on_failure_exception.py
+++ meiga/on_failure_exception.py
@@ -1,10 +1,14 @@
-from meiga import AnyResult
+from typing import TYPE_CHECKING
+
 from meiga.error import Error
+
+if TYPE_CHECKING:
+    from meiga.alias import AnyResult
 
 
 class OnFailureException(Error):
-    result: AnyResult
+    result: "AnyResult"
 
-    def __init__(self, result: AnyResult) -> None:
+    def __init__(self, result: "AnyResult") -> None:
         self.result = result
         Exception.__init__(self)
