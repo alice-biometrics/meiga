c4dc9b3002380806ebe861472039a7c16561cbed
acostapazo <acosta@alicebiometrics.com>
2023-05-13 15:57:08 +0200

    chore: add future annotations to modernize syntax

--- meiga/misc.py
+++ meiga/misc.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
+
 from typing import Any, List, cast
 
 
-def get_args_list(args: Any) -> List[Any]:
+def get_args_list(args: Any) -> list[Any]:
     if isinstance(args, tuple):
         list_args = list(args)
     else:
