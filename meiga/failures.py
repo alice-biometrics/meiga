1b10033124f37686365117505d48a278cfe84bcd
acostapazo <acosta@alicebiometrics.com>
2024-01-25 17:30:59 +0100

    Feature/add called from info on waiting for early return exception (#72)
    * feat(WaitingForEarlyReturn): add called from information to help on resolving missing early_return decorators
    
    * chore: modernize test_early_return
    
    * feat(WaitingForEarlyReturn): add fileneme to log hint
    
    * fix(python3.8): solving type compatibility

--- meiga/failures.py
+++ meiga/failures.py
@@ -1,4 +1,5 @@
-from typing import TYPE_CHECKING
+import inspect
+from typing import TYPE_CHECKING, Union
 
 from meiga.error import Error
 
@@ -8,15 +9,23 @@ if TYPE_CHECKING:
 
 class WaitingForEarlyReturn(Error):
     result: "AnyResult"
+    called_from: Union[str, None]
 
     def __init__(self, result: "AnyResult") -> None:
         self.result = result
+        try:
+            stack = inspect.stack()[2]
+            filename = stack.filename.split("/")[-1]
+            self.called_from = f"{stack[3]} on {filename}"
+        except:  # noqa
+            self.called_from = None
         Exception.__init__(self)
 
     def __str__(self) -> str:
+        function = f" ({self.called_from})" if self.called_from else ""
         return (
             f"This exception wraps the following result -> {self.result}"
-            f"\nIf you want to handle this error and return a Failure, please use early_return decorator on your function."
+            f"\nIf you want to handle this error and return a Failure, please use early_return decorator on your function{function}."
             f"\nMore info about how to use unwrap_or_return in combination with @early_return decorator on https://alice-biometrics.github.io/meiga/usage/result/#unwrap_or_return"
         )
 
--- tests/unit/test_waiting_for_early_return.py
+++ tests/unit/test_waiting_for_early_return.py
@@ -1,15 +1,26 @@
+import re
+from typing import Union
+
 import pytest
 
-from meiga import Error, Result, WaitingForEarlyReturn
+from meiga import AnyResult, Error, Failure, Result, WaitingForEarlyReturn, isSuccess
 from meiga.on_failure_exception import OnFailureException
 
 
-def expected_error(value: str) -> str:
-    return (
+def expected_error(
+    value: str, called_from: Union[str, None] = None, escape: bool = False
+) -> str:
+    called_from = f" ({called_from})" if called_from else ""
+
+    text = (
         f"This exception wraps the following result -> Result[status: failure | value: {value}]"
-        f"\nIf you want to handle this error and return a Failure, please use early_return decorator on your function."
+        f"\nIf you want to handle this error and return a Failure, please use early_return decorator on your function{called_from}."
         f"\nMore info about how to use unwrap_or_return in combination with @early_return decorator on https://alice-biometrics.github.io/meiga/usage/result/#unwrap_or_return"
     )
+    if escape:
+        return re.escape(text)  # necessary to match on pytest.raises contextmanager
+
+    return text
 
 
 @pytest.mark.unit
@@ -19,7 +30,9 @@ class TestWaitingForEarlyReturn:
 
         exception = WaitingForEarlyReturn(result)
 
-        assert expected_error("Error") == str(exception)
+        assert expected_error(
+            "Error", called_from="pytest_pyfunc_call on python.py"
+        ) == str(exception)
 
     def should_str_as_expected_an_exception(self):
         wrapped_exception = ValueError("Something went wrong")
@@ -27,7 +40,9 @@ class TestWaitingForEarlyReturn:
 
         exception = WaitingForEarlyReturn(result)
 
-        assert expected_error(wrapped_exception.__repr__()) == str(exception)
+        assert expected_error(
+            wrapped_exception.__repr__(), called_from="pytest_pyfunc_call on python.py"
+        ) == str(exception)
 
     def should_be_compatible_with_older_version_and_expect_st_as_expected_an_exception(
         self,
@@ -37,4 +52,39 @@ class TestWaitingForEarlyReturn:
 
         exception = OnFailureException(result)
 
-        assert expected_error(wrapped_exception.__repr__()) == str(exception)
+        assert expected_error(
+            wrapped_exception.__repr__(), called_from="pytest_pyfunc_call on python.py"
+        ) == str(exception)
+
+    def should_log_hint_when_called_from_function_and_not_early_return(self):
+        def inner_function() -> AnyResult:
+            result = Failure(Error())
+            result.unwrap_or_return()
+            return isSuccess
+
+        with pytest.raises(
+            WaitingForEarlyReturn,
+            match=expected_error(
+                "Error",
+                called_from="inner_function on test_waiting_for_early_return.py",
+                escape=True,
+            ),
+        ):
+            inner_function()
+
+    def should_log_hint_when_called_from_class_function_and_not_early_return(self):
+        class MyClass:
+            def execute(self) -> AnyResult:
+                result = Failure(Error())
+                result.unwrap_or_return()
+                return isSuccess
+
+        with pytest.raises(
+            WaitingForEarlyReturn,
+            match=expected_error(
+                "Error",
+                called_from="execute on test_waiting_for_early_return.py",
+                escape=True,
+            ),
+        ):
+            MyClass().execute()
